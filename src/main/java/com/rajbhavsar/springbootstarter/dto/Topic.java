cf9bc9c77f73a3e92d726313b9d6ad63032c9fe2
Raj Bhavsar <rajbhavsar909@gmail.com>
2020-07-01 10:08:22 +0530

    API performs CRUD with static data stored in List.

---
+++ src/main/java/com/rajbhavsar/springbootstarter/dto/Topic.java
@@ -0,0 +1,49 @@
+package com.rajbhavsar.springbootstarter.dto;
+
+public class Topic
+{
+	private String topicId;
+	private String topicName;
+	private String description;
+
+	public Topic()
+	{
+	}
+
+	public Topic(final String topicId, final String topicName, final String description)
+	{
+		this.topicId = topicId;
+		this.topicName = topicName;
+		this.description = description;
+	}
+
+	public String getTopicId()
+	{
+		return topicId;
+	}
+
+	public void setTopicId(final String topicId)
+	{
+		this.topicId = topicId;
+	}
+
+	public String getTopicName()
+	{
+		return topicName;
+	}
+
+	public void setTopicName(final String topicName)
+	{
+		this.topicName = topicName;
+	}
+
+	public String getDescription()
+	{
+		return description;
+	}
+
+	public void setDescription(final String description)
+	{
+		this.description = description;
+	}
+}
